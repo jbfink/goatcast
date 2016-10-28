16c1f79cb0233d1df445f2a52433a4824d535757
jbfink <john.fink@gmail.com>
2016-10-27 21:12:14 -0400

    generates goatcast.mp3

--- goatcast.rb
+++ goatcast.rb
@@ -1,7 +1,13 @@
 #!/usr/bin/env ruby
+require 'espeak'
 require 'csv'
+include ESpeak
 number_of_tweets = 15
 goat = CSV.read("./twitter-dump/tweets.csv")
+goatcast = []
 (1..number_of_tweets).each do |i|
-puts goat.sample[5]
+
+goatcast << goat.sample[5]
 end
+speech = Speech.new(goatcast, voice: "en")
+speech.save("goatcast.mp3")
